dd31f9c3aad6193d5e1de27bb9c2fa7eba63388c
Tom <tom@163.com>
2015-11-27 10:17:00 +0800

    modify logo pic

--- src/com/coldweather/app/activity/WeatherActivity.java
+++ src/com/coldweather/app/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package com.coldweather.app.activity;
 
 import com.coldweather.app.R;
+import com.coldweather.app.service.AutoUpdateService;
 import com.coldweather.app.util.HttpCallbackListener;
 import com.coldweather.app.util.HttpUtil;
 import com.coldweather.app.util.Utility;
@@ -141,5 +142,8 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 }
